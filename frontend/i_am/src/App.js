29aad959e6348e3255b9d72c0d00a3df7c561ecf
sana <sanalayouni41.@gmail.com>
2025-09-08 14:11:23 +0100

    deploy

---
+++ frontend/i_am/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import BusinessCardManager from './components/BusinessCardManager';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BusinessCardManager />
+    </div>
+  );
+}
+
+export default App;
